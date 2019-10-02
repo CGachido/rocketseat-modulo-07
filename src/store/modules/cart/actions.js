be95c7d4346c9ccba70720cafda482448a471812
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2019-10-02 16:13:47 -0300

    Starting with redux - reduz saga

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,7 +1,15 @@
-export function addToCart(product) {
-  return { type: '@cart/ADD', product };
+export function addToCartRequest(id) {
+  return { type: '@cart/ADD_REQUEST', id };
+}
+
+export function addToCartSuccess(product) {
+  return { type: '@cart/ADD_SUCCESS', product };
 }
 
 export function removeFromCart(id) {
   return { type: '@cart/REMOVE', id };
+}
+
+export function updateAmount(id, amount) {
+  return { type: '@cart/UPDATE_AMOUNT', id, amount };
 }
